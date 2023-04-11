36a43dbfdf633e5ab3868b5a4cfda647c4de250d
blobb <89486466+blobb999@users.noreply.github.com>
2023-04-11 10:59:59 +0200

    Update playlistbuilder.py
    adding .mkv and .avi Formats
    change logic to combine all playlist in each directory above.

--- playlistbuilder.py
+++ playlistbuilder.py
@@ -6,6 +6,11 @@ from tkinter import Tk, Button, Label, Entry, StringVar
 from urllib.parse import quote
 from xml.etree.ElementTree import Element, SubElement, tostring
 from xml.dom import minidom
+from natsort import natsorted
+
+
+def natural_sort_key(s):
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', s)]
 
 
 def delete_old_playlists(directory):
@@ -15,13 +20,8 @@ def delete_old_playlists(directory):
                 os.remove(os.path.join(root, file))
 
 
-def natural_sort_key(s):
-    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', s)]
-
-
 def create_playlist(directory):
-    media_files = sorted([f for f in os.listdir(directory) if f.endswith('.mp4') or f.endswith('.mp3')],
-                         key=natural_sort_key)
+    media_files = sorted([f for f in os.listdir(directory) if f.endswith(('.mp4', '.mp3', '.mkv', '.avi'))], key=natural_sort_key)
 
     if not media_files:
         return 0
@@ -64,7 +64,59 @@ def create_playlists_recursively(directory):
             playlist_count += 1
             file_count += files_added
 
+    combine_playlists(directory)
+
     messagebox.showinfo("Result", f"{playlist_count} playlists created with a total of {file_count} files.")
+
+
+def combine_playlists(parent_directory):
+    combined_playlist_count = 0
+
+    for root, dirs, files in os.walk(parent_directory, topdown=False):
+        playlist_files = [file for file in files if file.endswith('.xspf')]
+
+        if len(playlist_files) > 1:
+            combined_tracks = []
+
+            for file in playlist_files:
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    xml_tree = minidom.parseString(content)
+                    track_list = xml_tree.getElementsByTagName('trackList')[0]
+                    tracks = track_list.getElementsByTagName('track')
+
+                    for track in tracks:
+                        location = track.getElementsByTagName('location')[0]
+                        track_et = Element('track')
+                        location_et = SubElement(track_et, 'location')
+                        location_et.text = location.firstChild.data
+                        combined_tracks.append(track_et)
+
+            combined_tracks = natsorted(combined_tracks, key=lambda t: t.find('location').text)
+            playlist = Element('playlist', {'version': '1', 'xmlns': 'http://xspf.org/ns/0/'})
+            title = SubElement(playlist, 'title')
+            title.text = 'Playlist'
+            track_list = SubElement(playlist, 'trackList')
+
+            for track in combined_tracks:
+                track_list.append(track)
+
+            xml_string = tostring(playlist, 'utf-8')
+            pretty_xml = minidom.parseString(xml_string).toprettyxml(indent='  ')
+
+            folder_name = os.path.basename(root)
+            # Speichere die kombinierte Playlist im übergeordneten Verzeichnis
+            parent_folder = os.path.dirname(root)
+            combined_playlist_filename = os.path.join(parent_folder, f'{folder_name}.xspf')
+
+            with open(combined_playlist_filename, 'w', encoding='utf-8') as f:
+                f.write(pretty_xml)
+
+            combined_playlist_count += 1
+
+    messagebox.showinfo("Result", f"{combined_playlist_count} combined playlists created.")
+
 
 
 def browse_directory():
@@ -90,7 +142,7 @@ def main():
     folder_entry = Entry(root, textvariable=folder_path, width=50)
     folder_entry.pack(pady=10)
 
-    create_button = Button(root, text="Create Playlists", command=lambda: create_playlists_recursively(folder_path.get()))
+    create_button = Button(root, text="Create Playlists", command=lambda: [create_playlists_recursively(folder_path.get()), combine_playlists(folder_path.get())])
     create_button.pack(pady=10)
 
     root.mainloop()
